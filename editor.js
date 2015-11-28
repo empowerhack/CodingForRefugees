323dc13874742ff63d44138530879574c7ff9d79
Rebecca Appleyard <rklappleyard@gmail.com>
2015-11-28 16:55:55 +0000

    Saving for now - changing the idea so probably won't need this

--- editor.js
+++ editor.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   var editor = ace.edit("editor");
   editor.setTheme("ace/theme/monokai");
-  editor.getSession().setMode("ace/mode/javascript");
+  editor.getSession().setMode("ace/mode/python");
+
+  $('.getCode').click(function() {
+    console.log(editor.getValue());
+  });
 });
